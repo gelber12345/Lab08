27c84e09e8113b5b2a6c85b5a4df21060090682f
gelber12345 <gmartinezpas@unsa.edu.pe>
2022-07-01 19:57:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab08/FirebaseMessagesService.kt
@@ -0,0 +1,89 @@
+package com.example.lab08
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class FirebaseMessagesService: FirebaseMessagingService() {
+    companion object {
+        private const val TAG = "FCM Notification"
+        const val DEFAULT_NOTIFICATION_ID = 0
+    }
+
+    override fun onNewToken(token: String) {
+        Log.i(TAG, "new FCM token created: $token")
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        initNotificationChannel(notificationManager)
+    }
+    private fun getCustomDesign(title: String, message: String): RemoteViews? {
+        val remoteViews = RemoteViews(
+            applicationContext.packageName,
+            R.layout.notification
+        )
+        remoteViews.setTextViewText(R.id.title, title + "hola")
+        remoteViews.setTextViewText(R.id.message, message)
+        remoteViews.setImageViewResource(R.id.icon, R.drawable.ic_launcher_foreground)
+        Log.i("GRA", "GRAFIQUE")
+        return remoteViews
+    }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        var notificationBuilder = if (Build.VERSION_CODES.O <= Build.VERSION.SDK_INT) {
+            NotificationCompat.Builder(applicationContext, "1")
+        } else {
+            NotificationCompat.Builder(applicationContext)
+        }
+        /*notificationBuilder = notificationBuilder
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+        initNotificationChannel(notificationManager)
+        notificationManager.notify(DEFAULT_NOTIFICATION_ID, notificationBuilder.build())*/
+        notificationBuilder = notificationBuilder
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setAutoCancel(true)
+        notificationBuilder.setCustomContentView(getCustomDesign(title!!,body!!))
+
+        initNotificationChannel(notificationManager)
+        notificationManager.notify(DEFAULT_NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun initNotificationChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION_CODES.O <= Build.VERSION.SDK_INT) {
+            notificationManager.createNotificationChannelIfNOtExists(
+                channelId = "1",
+                channelName = "Default"
+            )
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun NotificationManager.createNotificationChannelIfNOtExists(
+    channelId: String,
+    channelName: String,
+    importance: Int = NotificationManager.IMPORTANCE_DEFAULT
+) {
+    var channel = this.getNotificationChannel(channelId)
+
+    if (channel == null) {
+        channel = NotificationChannel(
+            channelId,
+            channelName,
+            importance
+        )
+        this.createNotificationChannel(channel)
+    }
+}
